7962f4c1bb61ec4e577b797fec0eb9aa5469c96f
mehwishshakeel <mehwishshakeel>
2024-03-26 23:11:18 +0500

    If and Else statement

---
+++ Q26.js
@@ -0,0 +1,16 @@
+let alian_Color = "green";
+// First version
+if (alian_Color === "green") {
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+// second version
+if (alian_Color === "blue") {
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+export {};
---
+++ Q26.ts
@@ -0,0 +1,15 @@
+let alian_Color = "green";
+// First version
+if(alian_Color ==="green"){
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else{
+    console.log("player just earned 10 points.");
+}
+// second version
+if(alian_Color ==="blue"){
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else{
+    console.log("player just earned 10 points.");
+}
